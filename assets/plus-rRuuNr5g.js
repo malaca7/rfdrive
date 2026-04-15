c562312221b0f52e1d0ab21a34d3ae0a4a5c812a
malaca7 <rogeriosantanajr@gmail.com>
2026-04-14 23:55:21 -0300

    Updates

--- assets/plus-rRuuNr5g.js
+++ assets/plus-rRuuNr5g.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-4b75YMaQ.js";/**
+import{c as e}from"./index-C7u-Blpv.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
