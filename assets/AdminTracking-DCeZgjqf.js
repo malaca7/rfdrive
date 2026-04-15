a2f678e93ae0efc8860ce1c87b05d881a29ee1f2
malaca7 <rogeriosantanajr@gmail.com>
2026-04-15 17:38:27 -0300

    Updates

--- assets/AdminTracking-DCeZgjqf.js
+++ assets/AdminTracking-DCeZgjqf.js
@@ -1,4 +1,4 @@
-import{b as k,j as e}from"./vendor-query-H1L8k72B.js";import{c as w,s as x}from"./index-CJiK0pLx.js";import{C as r,d as l}from"./card-B-T0J6cf.js";import{B as u,U as _}from"./tabs-HfvueReb.js";import{m as M}from"./vendor-motion-qkyNbgk5.js";import{C,a as R}from"./table-properties-zuPJlian.js";import{M as $}from"./map-pinned-BFJrwu49.js";import{M as y}from"./map-pin-pBy7BbtR.js";import"./vendor-react-CjgwJ-mG.js";import"./vendor-supabase-6nRBcVya.js";/**
+import{b as k,j as e}from"./vendor-query-H1L8k72B.js";import{c as w,s as x}from"./index-CY5H3Vms.js";import{C as r,d as l}from"./card-BcRt_02W.js";import{B as u,U as _}from"./tabs-C3pqx4mY.js";import{m as M}from"./vendor-motion-qkyNbgk5.js";import{C,a as R}from"./table-properties-DtO69PrI.js";import{M as $}from"./map-pinned-gptnsGgP.js";import{M as y}from"./map-pin-BuJvmDTk.js";import"./vendor-react-CjgwJ-mG.js";import"./vendor-supabase-6nRBcVya.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/phone-DMD3k4Sh.js
+++ assets/phone-DMD3k4Sh.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CJiK0pLx.js";/**
+import{c as a}from"./index-CY5H3Vms.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
