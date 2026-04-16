878f0b7cf4a1f94d39e9867e22e2e2812cc09e2c
malaca7 <rogeriosantanajr@gmail.com>
2026-04-16 10:57:04 -0300

    Updates

---
+++ assets/index-Bc46ycey.js
@@ -0,0 +1,1 @@
+import{r as s,c as u}from"./vendor-react-iNZsg3tm.js";import{t as e}from"./index-DKUUjW5J.js";var c=u[" useId ".trim().toString()]||(()=>{}),f=0;function p(t){const[r,a]=s.useState(c());return e(()=>{t||a(o=>o??String(f++))},[t]),t||(r?`radix-${r}`:"")}export{p as u};
