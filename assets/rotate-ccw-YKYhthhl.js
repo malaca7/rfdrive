e0fa1759b05302a52a158f83114ad3f2d210090d
malaca7 <rogeriosantanajr@gmail.com>
2026-04-16 00:01:40 -0300

    Updates

--- assets/rotate-ccw-YKYhthhl.js
+++ assets/rotate-ccw-YKYhthhl.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-B4Nv84vR.js";/**
+import{c as t}from"./index-Cvmh2dgb.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
