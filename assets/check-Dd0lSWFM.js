63a08b2665891bdddb71e8c708b449512b57f25f
malaca7 <rogeriosantanajr@gmail.com>
2026-04-16 18:20:02 -0300

    Updates

---
+++ assets/check-Dd0lSWFM.js
@@ -0,0 +1,6 @@
+import{c}from"./index-C5iAWO1W.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=c("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{o as C};
