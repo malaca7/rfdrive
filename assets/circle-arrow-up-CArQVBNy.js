7d019257070dcd580fc22accfd43833d76ee05da
malaca7 <rogeriosantanajr@gmail.com>
2026-04-21 11:57:34 -0300

    Updates

---
+++ assets/circle-arrow-up-CArQVBNy.js
@@ -0,0 +1,6 @@
+import{e as r}from"./index-DYdw68KA.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=r("CircleArrowUp",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m16 12-4-4-4 4",key:"177agl"}],["path",{d:"M12 16V8",key:"1sbj14"}]]);export{c as C};
