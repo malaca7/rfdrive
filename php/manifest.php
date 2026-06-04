16e88b989743f30f0784e80fcea2a4b1b7212108
malaca7 <rogeriomalaquiasjr@gmail.com>
2026-06-04 09:02:27 -0300

    Updates

--- php/manifest.php
+++ php/manifest.php
@@ -28,6 +28,9 @@ if ($result && $row = $result->fetch_assoc()) {
 
 $conn->close();
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 header('Content-Type: application/json; charset=UTF-8');
 echo json_encode([
   'name' => $name,
